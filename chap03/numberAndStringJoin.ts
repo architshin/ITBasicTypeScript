2800e3e06ff9085274ecf317535a3b3fcca22d3d
Shinzo SAITO <architshin@websarva.com>
2021-01-31 03:39:53 +0900

    サンプル9追加。

---
+++ chap03/numberAndStringJoin.ts
@@ -0,0 +1,13 @@
+export{}
+
+const str = "結果";
+const num1 = 123;
+const num2 = 456;
+
+const ans1 = num1 + num2 + str;
+const ans2 = str + num1 + num2;
+const ans3 = num1 + num2 + str + num1 + num2;
+
+console.log(ans1);
+console.log(ans2);
+console.log(ans3);
