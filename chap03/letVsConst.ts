122ccd6754658989c39a62743ff47f3f0ea67902
Shinzo SAITO <architshin@websarva.com>
2021-02-10 18:04:01 +0900

    Chapter3のサンプルを追加。 (#2)
    * サンプル1追加。
    
    * サンプル2追加。
    
    * サンプル3追加。
    
    * サンプル4追加。
    
    * exportおまじないの追加。
    
    * サンプル5追加。
    
    * サンプル6追加。
    
    * サンプル7追加。
    
    * サンプル8追加。
    
    * サンプル9追加。
    
    * サンプル10追加。
    
    * サンプル11の追加。
    
    * 問1の解答追加。
    
    * 問2の解答追加。
    
    * 問3の解答追加。
    
    * 問4の解答追加。
    
    * 問5の解答追加。

---
+++ chap03/letVsConst.ts
@@ -0,0 +1,10 @@
+export{}
+
+let num1: number = 56;
+const num2: number = 35;
+console.log(num1);
+console.log(num2);
+num1 = 28;
+// num2 = 18;
+console.log(num1);
+console.log(num2);
---
+++ chap03/variables.ts
@@ -0,0 +1,7 @@
+export{}
+
+let hisName = "田中";
+console.log(hisName);
+hisName = "中野";
+console.log(hisName);
+// hisName = 789;
