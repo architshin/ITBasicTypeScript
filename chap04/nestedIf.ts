41fd1b0af468b2ae01c4b008aa113e3e1703865f
Shinzo SAITO <architshin@websarva.com>
2021-02-14 03:44:39 +0900

    余分な空白の削除。

--- chap04/nestedIf.ts
+++ chap04/nestedIf.ts
@@ -4,7 +4,7 @@ const num = Math.round(Math.random() * 100);
 console.log(`numの値: ${num}`);
 if((num % 2) == 0) {
 	console.log("2の倍数");
-	if((num % 3)  == 0) {
+	if((num % 3) == 0) {
 		console.log("3の倍数");
 	}
 } else {
