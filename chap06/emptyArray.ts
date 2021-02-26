381b3554394480a08f5776bbcc237ed7dc87e8ba
Shinzo SAITO <architshin@websarva.com>
2021-02-27 03:19:39 +0900

    サンプル7を原稿にあわえて変更。

--- chap06/emptyArray.ts
+++ chap06/emptyArray.ts
@@ -5,7 +5,7 @@ nameList[0] = "中田";
 nameList[1] = "田村";
 nameList[2] = "村井";
 nameList[3] = "井上";
-nameList[4] = "上野";
+nameList[4] = "上田";
 for(let i = 0; i < nameList.length; i++) {
 	console.log(`インデックス${i}: ${nameList[i]}`);
 }
