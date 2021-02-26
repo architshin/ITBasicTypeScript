de10323686eecbb78d62d12564a6d76cec82402e
Shinzo SAITO <architshin@websarva.com>
2021-02-27 01:21:52 +0900

    サンプル7追加。

---
+++ chap06/emptyArray.ts
@@ -0,0 +1,11 @@
+export{}
+
+const nameList: string[] = [];
+nameList[0] = "中田";
+nameList[1] = "田村";
+nameList[2] = "村井";
+nameList[3] = "井上";
+nameList[4] = "上野";
+for(let i = 0; i < nameList.length; i++) {
+	console.log(`インデックス${i}: ${nameList[i]}`);
+}
