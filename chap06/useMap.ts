2d5cac71cf1eead5b9f12f4ab9f3cffbff7b3578
Shinzo SAITO <architshin@websarva.com>
2021-02-27 04:25:21 +0900

    サンプル8を原稿に合わせて変更。

--- chap06/useMap.ts
+++ chap06/useMap.ts
@@ -1,12 +1,18 @@
 export{}
 
-const nameList: {[key: number]: string;} = {};
-nameList[349] = "中田";
-nameList[451] = "田村";
-nameList[122] = "村井";
-nameList[156] = "井上";
+const nameList: {[key: number]: string;} =
+{
+	349: "中田", 
+	451: "田村", 
+	122: "村井", 
+	156: "井上", 
+	556: "上田"
+};
+for(let key in nameList) {
+	console.log(`ID番号${key}: ${nameList[key]}`);
+}
 nameList[556] = "上野";
-
+nameList[551] = "野宮";
 for(let key in nameList) {
 	console.log(`ID番号${key}: ${nameList[key]}`);
 }
