a3f73530de0025d700fc9001ffb89bcb78f6faff
Shinzo SAITO <architshin@websarva.com>
2021-03-10 17:45:13 +0900

    Chapter6のサンプルを追加。 (#5)
    * サンプル1追加。
    
    * サンプル2追加。
    
    * サンプル3追加。
    
    * サンプル4追加。
    
    * サンプル5追加。
    
    * サンプル6追加。
    
    * サンプル7追加。
    
    * サンプル8追加。
    
    * サンプル6を原稿に合わせて変更。
    
    * サンプル7を原稿にあわえて変更。
    
    * サンプル8を原稿に合わせて変更。
    
    * サンプル8を修正。
    
    * サンプル9追加。
    
    * サンプル9を変更。
    
    * サンプル10追加。
    
    * サンプル11追加。
    
    * サンプル12追加。
    
    * 練習問題1追加。
    
    * 練習問題2追加。
    
    * 練習問題3を追加。
    
    * 練習問題4追加。

---
+++ chap06/arrayIndex.ts
@@ -0,0 +1,16 @@
+export{}
+
+const nameList: string[] = ["中田", "田村", "村井", "井上", "上田"];
+for(let i = 0; i < nameList.length; i++) {
+	console.log(`インデックス${i}: ${nameList[i]}`);
+}
+nameList[4] = "上野";
+nameList[5] = "野宮";
+for(let i = 0; i < nameList.length; i++) {
+	console.log(`インデックス${i}: ${nameList[i]}`);
+}
+console.log(`インデックス${6}: ${nameList[6]}`);
+nameList[8] = "宮本";
+for(let i = 0; i < nameList.length; i++) {
+	console.log(`インデックス${i}: ${nameList[i]}`);
+}
