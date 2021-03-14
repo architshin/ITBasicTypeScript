b0b9ca5405aa3b546898035a581ed44297d8c0fd
Shinzo SAITO <architshin@websarva.com>
2021-03-14 15:19:28 +0900

    Update useRestParameters.ts

--- chap07/useRestParameters.ts
+++ chap07/useRestParameters.ts
@@ -1,6 +1,6 @@
 export{}
 
-function sumScore(...scores: number[]): number {
+function sumScores(...scores: number[]): number {
 	let total = 0;
 	for(const score of scores) {
 		total += score;
@@ -8,7 +8,7 @@ function sumScore(...scores: number[]): number {
 	return total;
 }
 
-const total6 = sumScore(10, 9, 9, 10, 8, 9);
+const total6 = sumScores(10, 9, 9, 10, 8, 9);
 console.log(`6人の審査員の合計点: ${total6}`);
-const total4 = sumScore(10, 9, 8, 9);
+const total4 = sumScores(10, 9, 8, 9);
 console.log(`4人の審査員の合計点: ${total4}`);
