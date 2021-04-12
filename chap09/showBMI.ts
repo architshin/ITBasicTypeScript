c0d7179d16c4ec7c67a3c010915671e3cca62656
Shinzo SAITO <architshin@websarva.com>
2021-04-12 17:51:24 +0900

    Chapter9のサンプルを追加。 (#8)
    * サンプル1追加。
    
    * サンプル2追加。
    
    * サンプル3追加。
    
    * サンプル4追加。
    
    * サンプル5追加。
    
    * サンプル6追加。
    
    * サンプル7追加。
    
    * サンプル8追加。
    
    * サンプル9追加。
    
    * サンプル10追加。
    
    * サンプル11追加。
    
    * 問5までの練習問題追加。
    
    * 練習問題9-5追加。
    
    * 小数の四捨五入を追加。
    
    * 合計値計算をプロパティに任せたコードに変更。
    
    * 余分な空行を削除。
    
    * 削除サンプルを削除。

---
+++ chap09/showBMI.ts
@@ -0,0 +1,29 @@
+export{}
+
+class BodyMass {
+	name: string = "";
+	weight: number = 0;
+	height: number = 0;
+
+	constructor(name: string, weight: number, height: number) {
+		this.name = name;
+		this.weight = weight;
+		this.height = height;
+	}
+
+	showBMI() {
+		let bmi = this.weight / (this.height/100) / (this.height/100);
+		bmi = Math.round(bmi * 10) / 10;
+		console.log(`${this.name}さんのBMI値: ${bmi}`);
+	}
+	
+	showIdealWeight() {
+		let idealWeight = 22 *  (this.height/100) * (this.height/100);
+		idealWeight = Math.round(idealWeight);
+		console.log(`${this.name}さんの理想体重: ${idealWeight}kg`);
+	}
+}
+
+const nakatani = new BodyMass("中谷和弘", 68.4, 171.4);
+nakatani.showBMI();
+nakatani.showIdealWeight();
