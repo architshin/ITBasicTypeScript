887459cd931a8bbe69d869d30334b33de3b7f6c9
Shinzo SAITO <architshin@websarva.com>
2021-05-10 00:46:41 +0900

    データ取得まで完成。

--- chap12/main.ts
+++ chap12/main.ts
@@ -1,11 +1,14 @@
 import fetch from "node-fetch";
 
-const weatherinfoUrl = "https://openweathermap.org/data/2.5/weather?lang=ja";
+async function receiveWeatherInfo(url: string) {
+	const response = await fetch(url);
+	const weatherInfoJSON = await response.json();
+	console.log(weatherInfoJSON);
+}
+
+const weatherinfoUrl = "http://api.openweathermap.org/data/2.5/weather?lang=ja";
 const appId = "913136635cfa3182bbe18e34ffd44849";
 let q = "Himeji";
 
 const urlFull = `${weatherinfoUrl}&q=${q}&appid=${appId}`;
-
-
-const promise = fetch(urlFull);
-console.log(promise);
+receiveWeatherInfo(urlFull);
