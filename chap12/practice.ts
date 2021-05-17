5235f81e5d26615d386111d71897c7746403c8d0
Shinzo SAITO <architshin@websarva.com>
2021-05-17 20:53:23 +0900

    APIキーを削除。

--- chap12/practice.ts
+++ chap12/practice.ts
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 async function receiveWeatherInfo(): Promise<string> {
 	const weatherinfoUrl = "http://api.openweathermap.org/data/2.5/onecall?lang=ja&lat=35&lon=135&appid=";
-	const appId = "913136635cfa3182bbe18e34ffd44849";
+	const appId = "";
 	const url = weatherinfoUrl + appId;
 	const response = await fetch(url);
 	const responseText = response.text();
