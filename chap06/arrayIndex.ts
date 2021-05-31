9ac59e64619a87dc89738d5d38fcd5c2da6bebb0
Shinzo SAITO <architshin@websarva.com>
2021-05-31 21:02:02 +0900

    Update arrayIndex.ts

--- chap06/arrayIndex.ts
+++ chap06/arrayIndex.ts
@@ -1,16 +1,10 @@
 export{}
 
 const nameList: string[] = ["中田", "田村", "村井", "井上", "上田"];
-for(let i = 0; i < nameList.length; i++) {
-	console.log(`インデックス${i}: ${nameList[i]}`);
-}
+console.log(nameList);
 nameList[4] = "上野";
 nameList[5] = "野宮";
-for(let i = 0; i < nameList.length; i++) {
-	console.log(`インデックス${i}: ${nameList[i]}`);
-}
+console.log(nameList);
 console.log(`インデックス${6}: ${nameList[6]}`);
 nameList[8] = "宮本";
-for(let i = 0; i < nameList.length; i++) {
-	console.log(`インデックス${i}: ${nameList[i]}`);
-}
+console.log(nameList);
