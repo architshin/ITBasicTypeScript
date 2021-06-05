4de341689a2383e7b74dc4979b06ed49b796320f
Shinzo SAITO <architshin@websarva.com>
2021-06-05 17:07:19 +0900

    Chapter4のサンプルを本脱稿に合わせて変更。

--- chap04/conditionalOperator.ts
+++ chap04/conditionalOperator.ts
@@ -2,7 +2,7 @@ export { }
 
 const num = Math.round(Math.random() * 100);
 console.log(`numの値: ${num}`);
-if((num % 2) == 0 && (num % 3) == 0) {
+if(num % 2 == 0 && num % 3 == 0) {
 	console.log("2の倍数であり、しかも3の倍数");
 } else {
 	console.log("それ以外");
--- chap04/nestedIf.ts
+++ chap04/nestedIf.ts
@@ -2,9 +2,9 @@ export { }
 
 const num = Math.round(Math.random() * 100);
 console.log(`numの値: ${num}`);
-if((num % 2) == 0) {
+if(num % 2 == 0) {
 	console.log("2の倍数");
-	if((num % 3) == 0) {
+	if(num % 3 == 0) {
 		console.log("3の倍数");
 	}
 } else {
