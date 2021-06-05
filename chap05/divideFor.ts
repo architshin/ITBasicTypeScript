4fd1605bc311b8e3b4f9ebddc1d5ffc97f461205
Shinzo SAITO <architshin@websarva.com>
2021-06-05 21:04:38 +0900

    練習問題1を変更。

--- chap05/divideFor.ts
+++
@@ -1,8 +0,0 @@
-export{}
-
-for(let i = 1; i <= 10; i++) {
-	const num = Math.round(Math.random() * 10);
-	console.log(`${i}個目の乱数: ${num}`);
-	const ans = num / i;
-	console.log(`割り算の結果: ${ans}`);
-}
