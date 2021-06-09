a14654e879df83ecbc2ca9168414876877446063
Shinzo SAITO <architshin@websarva.com>
2021-06-09 20:53:05 +0900

    原稿内容に合わせてファイル名と内容を変更。

--- chap08/execFuncExpression.ts
+++ chap08/execFuncExpression.ts
@@ -1,9 +1,8 @@
 export{}
 
-function concatNameWithSpace(lastName: string, firstName: string): string {
+const func =  function(lastName: string, firstName: string): string {
 	return `${lastName} ${firstName}`;
 }
 
-const func = concatNameWithSpace;
 const result = func("田中", "太郎");
 console.log(result);
