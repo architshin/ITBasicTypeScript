d6a537fdc5fb9f841ce99fcea1332251a3fa55ca
Shinzo SAITO <architshin@websarva.com>
2021-06-11 20:38:50 +0900

    サンプル1を分割。

--- chap10/useExtends2.ts
+++ chap10/useExtends2.ts
@@ -1,43 +1,25 @@
 export{}
 
-class Student {
-	// 名前と英数国の点数を表す各プロパティ。
-	protected _name: string = "";
-	protected _english: number = 0;
-	protected _math: number = 0;
-	protected _japanese: number = 0;
+class Greetings {
+	// 名前のプロパティ。
+	name: string = "";
 
-	// コンストラクタ。引数をプロパティに格納する。
-	constructor(name: string, english: number, math: number, japanese: number) {
-		this._name = name;
-		this._english = english;
-		this._math = math;
-		this._japanese = japanese;
+	// コンストラクタ。名前を受け取りプロパティに格納する。
+	constructor(name: string) {
+		this.name = name;
 	}
-	
-	// 3教科の合計点を表示するメソッド。
-	showScoresSum() {
-		const sum = this._english + this._math + this._japanese;
-		console.log(`${this._name}の合計得点: ${sum}`);
+
+	// 「こんにちは」と表示するメソッド。
+	sayHello() {
+		console.log(`${this.name}さん、こんにちは。`)
 	}
 }
 
-class Student5 extends Student {
-	private _science: number = 0;
-	private _social: number = 0;
-
-	constructor(name: string, english: number, math: number, japanese: number, science: number, social: number) {
-		super(name, english, math, japanese)
-		this._science = science;
-		this._social = social;
-	}
-
-	showScoresSum() {
-		super.showScoresSum();
-		const sum = this._english + this._math + this._japanese + this._science + this._social;
-		console.log(`${this._name}の5教科の合計得点: ${sum}`);
+class MyDo extends Greetings {
+	sayHello() {
+		console.log(`${this.name}さん、まいど。`)
 	}
 }
 
-const emi = new Student5("千原恵美", 85, 80, 90, 88, 83);
-emi.showScoresSum();
+const jiro = new MyDo("坂本次郎");
+jiro.sayHello();
