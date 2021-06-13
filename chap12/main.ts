6f29de649e54023e57912bf76208f8d674f4fb99
Shinzo SAITO <architshin@websarva.com>
2021-06-13 16:39:34 +0900

    Chapter12のサンプルを本脱稿に合わせて変更。

--- chap12/main.ts
+++ chap12/main.ts
@@ -4,21 +4,32 @@ import {receiveWeatherInfo} from "./weatherinfo-receiver";
 import {WeatherInfo} from "./WeatherInfo";
 
 //アクセス先URLの基本部分の変数を用意。
-const weatherinfoUrl = "http://api.openweathermap.org/data/2.5/weather?lang=ja";
-//APIキーの変数を用意。ここに各自の文字列を記述する!!
-const appId = "";
-//都市名を表す変数を用意。
-let q = "Himeji";
-
+const weatherinfoUrl = "http://api.openweathermap.org/data/2.5/weather";
+//クエリパラメータの元データとなるオブジェクトリテラルを用意。
+const params:{
+	lang: string,
+	q: string,
+	appId: string
+} =
+{
+	//言語設定のクエリパラメータ
+	lang: "ja",
+	//都市名を表すクエリパラメータ。
+	q: "Himeji",
+	//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
+	appId: "xxxxxxx"
+}
+//クエリパラメータを生成。
+const queryParams = new URLSearchParams(params);
 //実際にアクセスするURLを生成。
-const urlFull = `${weatherinfoUrl}&q=${q}&appid=${appId}`;
+const urlFull = `${weatherinfoUrl}?${queryParams}`;
 //receiveWeatherInfo関数を実行。
 const promise = receiveWeatherInfo(urlFull);
 //非同期処理が成功した場合の処理を定義。
 promise.then(
 	function(weatherInfo: WeatherInfo) {
 		//WeatherInfoオブジェクトから必要情報を取得して表示文字列を生成。
-		const message = `現在の${weatherInfo.cityName}の天気は、${weatherInfo.weatherDesc}です。\n緯度は${weatherInfo.latitude}で軽度は${weatherInfo.longitude}です。`;
+		const message = `現在の${weatherInfo.cityName}の天気は、${weatherInfo.weatherDesc}です。\n緯度は${weatherInfo.latitude}で経度は${weatherInfo.longitude}です。`;
 		//表示。
 		console.log(message);
 	}
